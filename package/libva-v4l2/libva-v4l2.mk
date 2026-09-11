0f99853a3b57533606f8795a6ebd09f952e8f070
lightelf76 <valerygaynullin@yandex.ru>
2026-09-11 18:45:52 +0300

    continue

--- package/libva-v4l2/libva-v4l2.mk
+++ package/libva-v4l2/libva-v4l2.mk
@@ -5,12 +5,12 @@
 ################################################################################
 
 #LIBVA_V4L2_DRIVER_VERSION = 74b336a
-LIBVA_V4L2_DRIVER_VERSION = master
-LIBVA_V4L2_DRIVER_SITE = https://github.com/mxsrc/libva-v4l2.git
-LIBVA_V4L2_DRIVER_SITE_METHOD = git
-LIBVA_V4L2_DRIVER_LICENSE = MIT
-LIBVA_V4L2_DRIVER_LICENSE_FILES = COPYING
-LIBVA_V4L2_DRIVER_DEPENDENCIES = host-pkgconf libdrm libva libudev
-LIBVA_V4L2_DRIVER_CONF_OPTS = -Dkernel_headers=$(STAGING_DIR)/usr/include
+LIBVA_V4L2_VERSION = master
+LIBVA_V4L2_SITE = https://github.com/mxsrc/libva-v4l2.git
+LIBVA_V4L2_SITE_METHOD = git
+LIBVA_V4L2_LICENSE = MIT
+LIBVA_V4L2_LICENSE_FILES = COPYING
+LIBVA_V4L2_DEPENDENCIES = host-pkgconf libdrm libva libudev
+LIBVA_V4L2_CONF_OPTS = -Dkernel_headers=$(STAGING_DIR)/usr/include
 
 $(eval $(meson-package))
